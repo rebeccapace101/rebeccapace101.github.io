2856a10da9856f54017047befd8ff2fa5d341ad3
Hannah Chung <hannah.y.chung@my.wheaton.edu>
2025-03-04 14:41:59 -0600

    testing pfp change (commented out for later)

--- profile.js
+++ profile.js
@@ -8,6 +8,8 @@ const email = document.getElementById("email");
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 const changeUser = document.getElementById("changeUser");
+const changePFP = document.getElementById("changePFP");
+
 const userNameBody = document.getElementById("userNameBody");
 const db = getFirestore(app);
 const signOutButton = document.getElementById("signOutButton");
@@ -41,55 +43,6 @@ onAuthStateChanged(auth, async (user) => {
         console.log("User is signed out");
     }
 });
-const popUpUser = document.getElementById("popupOverlayUser");
-const closePopupUser = document.getElementById("closePopupUser");
-
-const callNewUser = async () => {
-
-    popUpUser.style.display = "block";
-
-}
-
-const closeUserWindow = async () => {
-
-    popUpUser.style.display = "none";
-}
-
-closePopupUser.addEventListener('click', closeUserWindow);
-
-
-const userNameChange = async () => {
-
-    const inputField = document.getElementById("userNameInput").value;
-
-    onAuthStateChanged(auth, async (user) => {
-        popUpUser.style.display = "none";
-        if (user) {
-            updateProfile(user, { displayName: inputField }).then(() => {
-                userName.innerHTML = user.displayName;
-                userNameBody.innerHTML = user.displayName;
-            }).catch((error) => {
-                console.error("Error updating username ", error);
-            });
-        } else {
-            // User is signed out
-            console.log("User is signed out");
-        }
-
-    });
-
-}
-
-const userSignOut = async () => {
-    signOut(auth).then(() => {
-        alert("You have signed out successfully!");
-        window.location.href = "./index.html";
-    }).catch((error) => { })
-}
-
-changeUser.addEventListener('click', callNewUser)
-submitNewUsername.addEventListener('click', userNameChange);
-signOutButton.addEventListener('click', userSignOut);
 
 
 //for displaying and changing profile picture
@@ -122,4 +75,105 @@ onAuthStateChanged(auth, async (user) => {
         console.log("No user is signed in.");
     }
 });
+
+
+
+
+const popUpUser = document.getElementById("popupOverlayUser");
+const closePopupUser = document.getElementById("closePopupUser");
+
+const callNewUser = async () => {
+
+    popUpUser.style.display = "block";
+
+}
+
+const closeUserWindow = async () => {
+
+    popUpUser.style.display = "none";
+}
+
+closePopupUser.addEventListener('click', closeUserWindow);
+
+
+
+
+const userNameChange = async () => {
+
+    const inputField = document.getElementById("userNameInput").value;
+
+    onAuthStateChanged(auth, async (user) => {
+        popUpUser.style.display = "none";
+        if (user) {
+            updateProfile(user, { displayName: inputField }).then(() => {
+                userName.innerHTML = user.displayName;
+                userNameBody.innerHTML = user.displayName;
+            }).catch((error) => {
+                console.error("Error updating username ", error);
+            });
+        } else {
+            // User is signed out
+            console.log("User is signed out");
+        }
+
+    });
+
+}
+/*
+const popUpPFP = document.getElementById("popupOverlayPFP");
+const closePopupPFP = document.getElementById("closePopupPFP");
+
+const callNewPFP = async () => {
+
+    popUpPFP.style.display = "block";
+
+}
+
+const closePFPWindow = async () => {
+
+    popUpPFP.style.display = "none";
+}
+
+closePopupPFP.addEventListener('click', closePFPWindow);
+const pfpInput = document.getElementById("imageUpload");
+const newPFP = null;
+
+pfpInput.addEventListener('change', (event) => {
+    newPFP = event.target.files[0];
+});
+
+const pfpChange = async () => {
+
+    onAuthStateChanged(auth, async (user) => {
+        popUpPFP.style.display = "none";
+        if (user) {
+            updateProfile(user, { photoURL: newPFP }).then(() => {
+                profilePic.src = newPFP;
+            }).catch((error) => {
+                console.error("Error updating pfp ", error);
+            });
+        } else {
+            // User is signed out
+            console.log("User is signed out");
+        }
+
+    });
+
+}
+
+changePFP.addEventListener('click', callNewPFP);
+submitNewPFP.addEventListener('click', pfpChange);
+*/
+
+const userSignOut = async () => {
+    signOut(auth).then(() => {
+        alert("You have signed out successfully!");
+        window.location.href = "./index.html";
+    }).catch((error) => { })
+}
+
+changeUser.addEventListener('click', callNewUser)
+submitNewUsername.addEventListener('click', userNameChange);
+signOutButton.addEventListener('click', userSignOut);
+
 
