15798b0ae19ba299e539c6bf1eca63b83531a33c
Hannah Chung <hannah.y.chung@my.wheaton.edu>
2025-03-27 14:08:24 -0500

    refining accountability partner functions

--- accountability.js
+++ accountability.js
@@ -6,10 +6,15 @@ const db = getFirestore(app);
 
 const idInput = document.getElementById("idInput");
 const userNameDisplay = document.getElementById("userNameDisplay");
+const displayResults = document.getElementById("displayResults");
+const profilePic = document.getElementById("profile-pic");
+
+
 
 //function to look for a user based on user input
 const searchUser = async () => {
     const userId = idInput.value.trim();
+
     console.log("Entered User ID:", userId); // ✅ Debugging Log
     if (!userId) {
         alert("Please enter a valid User ID.");
@@ -22,7 +27,21 @@ const searchUser = async () => {
 
         if (userSnap.exists()) {
             const userData = userSnap.data();
-            userNameDisplay.innerHTML = `Username: ${userData.displayName || "Not Available"}`;
+            userNameDisplay.innerHTML = `${userData.displayName || "Not Available"}`;
+            const photoURL = userData.photoURL || auth.currentUser?.photoURL;
+
+            if (photoURL) {
+                profilePic.src = photoURL;
+                profilePic.style.display = "block"; // Show the image
+            } else {
+                console.log("No profile picture found.");
+                profilePic.style.display = "none"; // Hide if no image available
+            }
+
+            errorMessage.innerHTML = "";
+
+            displayResults.style.display = "block";
+
         } else {
             alert("User not found.");
         }
@@ -36,3 +55,38 @@ const searchUser = async () => {
 const submitID = document.getElementById("submitID");
 
 submitID.addEventListener('click', searchUser);
+
+
+//code to add an accountability partner
+
+const addPartner = document.getElementById("addPartner");
+const errorMessage = document.getElementById("errorMessage");
+
+const addAccountabilityPartner = async () => {
+    const user = auth.currentUser;
+
+    if (user) {
+        const userId = idInput.value.trim();
+
+        const userRef = doc(db, "users", userId);
+        const userSnap = await getDoc(userRef);
+        const userData = userSnap.data();
+        const userPrivacy = userData.privacy;
+
+        if (userPrivacy == "public") {
+            errorMessage.innerHTML = "";
+            userNameDisplay.innerHTML = "Successfully added!";
+            addPartner.style.display = "none";
+            profilePic.style.display = "none";
+
+        } else {
+            errorMessage.innerHTML = "This user is not public.";
+        }
+
+    } else {
+        console.log("No user is signed in.");
+    }
+
+}
+
+addPartner.addEventListener('click', addAccountabilityPartner);
