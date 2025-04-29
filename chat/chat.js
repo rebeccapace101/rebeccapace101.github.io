fc2f1a008a71dcd3a5f8ef639520919773bcfa92
rebeccapace101 <rebecca.pace101@gmail.com>
2025-04-28 19:59:04 -0500

    started chat html and js

---
+++ chat/chat.js
@@ -0,0 +1,13 @@
+import { app } from './init.mjs';
+import {
+    getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
+import {
+    getFirestore, doc, setDoc, arrayUnion, getDoc, updateDoc, collection, addDoc, getDocs
+} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+
+// Initialize Firebase
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+
