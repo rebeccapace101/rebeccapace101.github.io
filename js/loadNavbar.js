ac7bc0c649053055034907bdb05fdf402650d955
csnyder27 <156149730+csnyder27@users.noreply.github.com>
2025-04-28 22:58:14 -0500

    admin and Accountability added to nav bar

---
+++ js/loadNavbar.js
@@ -0,0 +1,5 @@
+// Simple import of the navbar component
+import { Navbar } from '../components/navbar/navbar.js';
+
+// Log to confirm loading
+console.log("loadNavbar.js: Navbar component loaded");
