c4e8434a5c90de57c1bf4ae852edb97802b784db
rebeccapace101 <rebecca.pace101@gmail.com>
2025-04-30 10:31:09 -0500

    started send message

--- chat/chat.js
+++ chat/chat.js
@@ -9,5 +9,35 @@ import {
 // Initialize Firebase
 const db = getFirestore(app);
 const auth = getAuth(app);
+const sendButton = document.getElementById("sendMessage");
+const messageBox = document.getElementById("message");
 
+//send chat message
+const sendMessage = async () => {
+     onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                const message = messageBox.value.trim();
+                if (!entryText) {
+                                console.log("message is empty.");
+                                return;
+                            }
+                
+                 try {
+                     const todayDate = getTodayDate();
+                     const messageRef = doc(db, "chat", user.uid, "messages", todayDate);
+                
+                    await setDoc(journalRef, {
+                        text: message,
+                        date: todayDate
+                       });
+                } catch (error) {
+                      console.error("Error sending message:", error);
+                }
+
+            } else {
+                console.log("User is signed out.");
+            }
+
+     });
+}
 
